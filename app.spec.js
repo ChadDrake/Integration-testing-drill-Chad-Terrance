3b68417bfdecdea077cc957d92bbbc3b091cc98c
Terrance IvyBrown <boxman06@yahoo.com>
2020-09-16 11:06:23 -0500

    First commit

---
+++ app.spec.js
@@ -0,0 +1,18 @@
+const  app = require("../app");
+const  mocha = require("mocha");
+const chai = require("chai");
+
+describe('GET/frequency', () => {
+it('should return and object');
+
+it('should return 400 if no string is sent');
+
+it('should count the number of unique characters');
+
+it('should return the character with the highest count in string');
+
+it('should average the number of characters');
+});
+
+
+
